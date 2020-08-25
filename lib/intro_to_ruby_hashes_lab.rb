d42f420fcfc52e5e83ce7cb962ffa4128d2c6e45
Roman Tetelbaum <rtetelbaum@mailbox.org>
2020-08-25 06:15:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  if hash.fetch
+  if !hash[key]
+    hash[key] = 1
+  else
+    hash[key] += 1
   end
   
   # given a hash an a key as parameters, return an updated hash
